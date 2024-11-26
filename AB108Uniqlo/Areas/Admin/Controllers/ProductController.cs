bbae0473d95239670506ba1c3c75ebbf252f351e
ulviabdullazada <ulvi.abdullazda@code.edu.az>
2024-11-26 11:03:40 +0400

    update product

--- AB108Uniqlo/Areas/Admin/Controllers/ProductController.cs
+++ AB108Uniqlo/Areas/Admin/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using Azure.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
 using System.Net.Mime;
 
 namespace AB108Uniqlo.Areas.Admin.Controllers
@@ -32,7 +33,7 @@ namespace AB108Uniqlo.Areas.Admin.Controllers
                 if (!vm.File.IsValidSize(400))
                     ModelState.AddModelError("File", "File must be less than 400kb");
             }
-            if (vm.OtherFiles.Any())
+            if (vm.OtherFiles != null && vm.OtherFiles.Any())
             {
                 if (!vm.OtherFiles.All(x=> x.IsValidType("image")))
                 {
@@ -58,13 +59,60 @@ namespace AB108Uniqlo.Areas.Admin.Controllers
             }
             Product product = vm;
             product.CoverImage = await vm.File!.UploadAsync(_env.WebRootPath,"imgs","products");
-            product.Images = vm.OtherFiles.Select(x => new ProductImage
+            product.Images = vm.OtherFiles?.Select(x => new ProductImage
             {
                 ImageUrl = x.UploadAsync(_env.WebRootPath, "imgs", "products").Result 
             }).ToList();
             await _context.Products.AddAsync(product);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id is null) return BadRequest();
+            var data = await _context.Products
+                .Where(x=> x.Id == id)
+                .Select(x=> new ProductUpdateVM
+                {
+                    Id = x.Id,
+                    BrandId = x.BrandId ?? 0,
+                    CostPrice = x.CostPrice,
+                    Description = x.Description,
+                    Discount = x.Discount,
+                    FileUrl = x.CoverImage,
+                    Name = x.Name,
+                    Quantity = x.Quantity,
+                    SellPrice = x.SellPrice,
+                    OtherFilesUrls = x.Images.Select(y=> y.ImageUrl)
+                })
+                .FirstOrDefaultAsync();
+            if (data is null) return NotFound();
+            ViewBag.Categories = await _context.Brands.Where(x=> !x.IsDeleted)
+                .ToListAsync();
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, ProductUpdateVM vm)
+        {
+            var data = await _context.Products.Include(x => x.Images)
+                .Where(x=> x.Id == id)
+                .FirstOrDefaultAsync();
+            data.Images.AddRange(vm.OtherFiles.Select(x => new ProductImage
+            {
+                ImageUrl = x.UploadAsync(_env.WebRootPath, "imgs", "products").Result
+            }).ToList());
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteImgs(int id, IEnumerable<string> imgNames)
+        {
+            int result = await _context.ProductImages.Where(x => imgNames.Contains(x.ImageUrl)).ExecuteDeleteAsync();
+            if(result > 0)
+            {
+                //serverden (komputerden (fayllardan)) kohne shekilleri sil
+            }
+            return RedirectToAction(nameof(Update), new { id });
         }
     }
 }
---
+++ AB108Uniqlo/ViewModels/Products/ProductUpdateVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AB108Uniqlo.ViewModels.Products
+{
+    public class ProductUpdateVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+        [Range(0, 100)]
+        public int Discount { get; set; }
+        public int BrandId { get; set; }
+        public string? FileUrl { get; set; }
+        public IFormFile File { get; set; }
+        public IEnumerable<string>? OtherFilesUrls { get; set; }
+        public ICollection<IFormFile>? OtherFiles { get; set; }
+    }
+}
