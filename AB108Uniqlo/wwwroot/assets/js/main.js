a205c5a0a454d64b323848d03e426ed9c22f1a61
ulviabdullazada <ulvi.abdullazda@code.edu.az>
2024-11-27 10:58:16 +0400

    Misa

--- AB108Uniqlo/wwwroot/assets/js/main.js
+++ AB108Uniqlo/wwwroot/assets/js/main.js
@@ -426,14 +426,14 @@ $('.image-popup').magnificPopup({
   $("#slider-range").slider({
       range: true,
       min: 10,
-      max: 500,
-      values: [110, 400],
+      max: 5000,
+      values: [$("#slider-range").attr('min-val') ?? 420, $("#slider-range").attr('max-val') ?? 3800],
       slide: function(event, ui) {
-          $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+          $("#amount").val(ui.values[0] + "-" + ui.values[1]);
       }
   });
-  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-      " - $" + $("#slider-range").slider("values", 1));
+  $("#amount").val($("#slider-range").slider("values", 0) +
+      "-" + $("#slider-range").slider("values", 1));
 
 
 
